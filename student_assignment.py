7ca871b182f4b58b7f7f9275d9654c2560817a9f
junsheng_wang <build@build-server>
2025-02-06 19:50:49 +0800

    hw02_1
    Signed-off-by: junsheng_wang <build@build-server>

--- student_assignment.py
+++ student_assignment.py
@@ -7,7 +7,13 @@ q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    loader = PyPDFLoader(file_path=q1_pdf)
+    docs = loader.load()
+    #text_splitter = CharacterTextSplitter(chunk_overlap=0)
+    #chunks = text_splitter.split_text(docs)
+    return docs[-1]
+    #pprint(docs[0])
+    #pass
 
 def hw02_2(q2_pdf):
     pass

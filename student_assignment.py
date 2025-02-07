891e2bfefc7ee2e44a983520e5382bc125d46f5c
junsheng_wang <build@build-server>
2025-02-07 19:29:24 +0800

    hw02_2 use func parms to implement
    Signed-off-by: junsheng_wang <build@build-server>

--- student_assignment.py
+++ student_assignment.py
@@ -23,16 +23,19 @@ def hw02_2(q2_pdf):
         text += docs[i].page_content
         if i != len(docs)-1:
             text +='\n'
-    text = re.sub(r'(\n[ ]*第[\s]*[一二三四五六七八九十\d]+[\s]*章[^\n]*\n)', r'\n\n\t\1', text)
+    #text = re.sub(r'(\n[ ]*第[\s]*[一二三四五六七八九十\d]+[\s]*章[^\n]*\n)', r'\n\n\t\1', text)
     #print(text)
-    text = re.sub(r'(\n[ ]*第[\s]*[一二三四五六七八九十\d-]+[\s]*條[^\n]*\n)', r'\n\n\t\1', text)
+    #text = re.sub(r'(\n[ ]*第[\s]*[一二三四五六七八九十\d-]+[\s]*條[^\n]*\n)', r'\n\n\t\1', text)
     #print(text)
-    text_splitter = RecursiveCharacterTextSplitter(separators=['\n\n\t'],
+    text_splitter = RecursiveCharacterTextSplitter(separators=['\n[ ]*第[\s]*[一二三四五六七八九十\d]+[\s]*章[^\n]*\n','\n[ ]*第[\s]*[一二三四五六七八九十\d-]+[\s]*條[^\n]*\n'],
                                                    chunk_size=10,
-                                                   chunk_overlap=0)
+                                                   chunk_overlap=0,
+                                                   is_separator_regex = True)
     #print(f'after text {text}')
     #print(docs[0])
     chunks = text_splitter.split_text(text)
+    #chunks = text_splitter.split_documents(docs)
+    #transform_documents
     # text_splitter = CharacterTextSplitter(chunk_overlap=0)
     # chunks = text_splitter.split_text(docs)
 
